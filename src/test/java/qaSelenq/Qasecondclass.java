0c2d1fb39f3edca201ebae9ec531e5e77ca84cd1
VaibhavGit <vaibhavbansude4627@gmail.com>
2023-10-30 10:21:08 +0530

    second commit

--- src/test/java/qaSelenq/Qasecondclass.java
+++ src/test/java/qaSelenq/Qasecondclass.java
@@ -5,6 +5,8 @@ public class Qasecondclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("welcome home");
+		System.out.println("vaibhav");
 	}
 
 }
